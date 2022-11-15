91c2cb012ce141fdda503bed21f6c28e3482a27d
Francoorellana <francoorellana1@gmail.com>
2022-11-15 10:01:42 -0300

    localstorage

--- script/script.js
+++ script/script.js
@@ -1,19 +1,18 @@
 var agregarAlCarrito = document.getElementsByName("agregar");
 var productosEnCarrito = document.getElementById("cantidadCarrito");
+
 var productoAgregado = 0;
+productosEnCarrito.innerHTML = productoAgregado;
 
+agregarAlCarrito.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        
+        productoAgregado++;
+        localStorage.setItem("productoAgregado", productoAgregado);
+        
+        var agregado = localStorage.getItem("productoAgregado");
+        productosEnCarrito.innerHTML = agregado;
 
-agregarAlCarrito.addEventListener("click",()=>{
-    productoAgregado++;
-    productosEnCarrito.innerHTML = productoAgregado;
-})
+    });
+});
 
-
-
-/*
-agregarAlCarrito.addEventListener("click",(e)=>{
-    console.log(agregarAlCarrito.value);
-
-})
-
-*/
