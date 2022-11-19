a90b8ac090f4e5e6c40c85fcfc1f8302d5d2276d
Francoorellana <francoorellana1@gmail.com>
2022-11-19 02:34:50 -0300

    hasta punto 3

---
+++ script/valuesCarrito.js
@@ -0,0 +1,33 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+const estadoCarrito = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+const contarCarrito = () => {
+    let carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", carritoLength);
+
+    let cantidadEnCarrito = document.getElementById("cantidadDeProductos");
+    cantidadEnCarrito.innerHTML = localStorage.getItem("carritoLength");
+}
+
+const buscarCategoria = () => {
+    
+    document.addEventListener("keyup", (e)=>{
+
+        if(e.target.matches("#buscar")){
+            document.querySelectorAll(".claseCategoria").forEach(categoria =>{
+                categoria.textContent.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+                ?categoria.classList.remove("filtro")
+                :categoria.classList.add("filtro")
+            });
+        }
+    });    
+
+}
+
+buscarCategoria();
+contarCarrito();
+estadoCarrito();
