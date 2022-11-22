e84e3127524205afefcc9c7d1dca0306065f1fd6
Francoorellana <francoorellana1@gmail.com>
2022-11-21 21:43:19 -0300

    hasta punto 4

--- script/direccion.js
+++ script/direccion.js
@@ -1,4 +1,5 @@
-const direcciones = JSON.parse(localStorage.getItem("direcciones"))  || [];
+let direcciones = JSON.parse(localStorage.getItem("direcciones")) || [];
+var direccionesActuales = document.getElementById("tablaDirecciones");
 const botonGuardar = document.getElementById('boton-guardar');
 var alias = document.querySelector("#alias");
 var direccion = document.querySelector("#direccion");
@@ -10,9 +11,9 @@ contador = direcciones.length;
 
 //guardando direccion
 function logicaGuardado() {
-  
+
   var nuevaDireccion = {
-    id : contador,
+    id: contador,
     alias: alias.value,
     direccion: direccion.value
   };
@@ -20,13 +21,60 @@ function logicaGuardado() {
   direcciones.push(nuevaDireccion);
 
 }
-botonGuardar.addEventListener("click", () => {
 
-  logicaGuardado();
+if (botonGuardar !== null) {
+  botonGuardar.addEventListener("click", () => {
+
+    logicaGuardado();
+    estadoUsers();
+
+  })
+}
+
+
+
+if (direccionesActuales !== null) {
+
+  function mostrarDirecciones ()  {
+
+    for (i = 0; i < direcciones.length; i++) {
+      if (direcciones[i].alias != undefined) {
+        direccionesActuales.innerHTML += `
+          <tbody >                         
+          
+          <tr> <td>${direcciones[i].alias}</td> <td>${direcciones[i].direccion} </td> <td name="eliminar" ><i  class="fa-regular fa-trash-can eliminar" style="cursor: pointer;"></i> </td> </tr> 
+          </tbody> 
+          `
+
+      }
+    }
+  }
+
+  mostrarDirecciones();
+
+}
+
+let eliminar = () => {
+
+  console.log("ingresa");
+  let foundId = direcciones.find((element) => element.id);
+  console.log(foundId);
+  direcciones = direcciones.filter((direccionesId) => {
+    return direccionesId !== foundId;
+  })
+  console.log(direcciones);
   estadoUsers();
+  mostrarDirecciones();
 
+}
+
+var botonEliminar = document.getElementsByName("eliminar");
+
+botonEliminar.forEach((item) => {
+  item.addEventListener("click", eliminar)
 })
 
-const estadoUsers = () => {
+var estadoUsers = () => {
   localStorage.setItem("direcciones", JSON.stringify(direcciones));
 }
+
--- script/viewAdress.js
+++ script/viewAdress.js
@@ -1,25 +1,43 @@
-const direccionesActuales = document.getElementById("tablaDirecciones");
-const direcciones = JSON.parse(localStorage.getItem("direcciones"))  || [];
+var direccionesActuales = document.getElementById("tablaDirecciones");
+var direcciones = JSON.parse(localStorage.getItem("direcciones"))  || [];
 
-for (i = 0; i < direcciones.length; i++) {
-    if(direcciones[i].alias != undefined){
-        direccionesActuales.innerHTML += `
-        <tbody >                         
-        
-        <tr> <td>${direcciones[i].alias}</td> <td>${direcciones[i].direccion} </td> <td><i id="eliminar" class="fa-regular fa-trash-can eliminar" style="cursor: pointer;"></i> </td> </tr> 
-        </tbody> 
-        `
 
-    }
+const mostrarDirecciones = () => {
+
+    for (i = 0; i < direcciones.length; i++) {
+        if(direcciones[i].alias != undefined){
+            direccionesActuales.innerHTML += `
+            <tbody >                         
+            
+            <tr> <td>${direcciones[i].alias}</td> <td>${direcciones[i].direccion} </td> <td name="eliminar" ><i  class="fa-regular fa-trash-can eliminar" style="cursor: pointer;"></i> </td> </tr> 
+            </tbody> 
+            `
+    
+        }
+    }    
 }
+mostrarDirecciones();
 
-
-const botonEliminar = document.getElementById("eliminar");
-if(botonEliminar != null){
-    botonEliminar.addEventListener("click", () => {
-        let newData = direcciones.filter(item => item.alias !== direcciones.alias)
-        console.log(newData);
-        /*localStorage.setItem('data', JSON.stringify(newData));
-        setProducts(newData)*/
+function eliminar() {
+    localStorage.removeItem("direcciones");
+    console.log("ingresa");
+    var foundId = direcciones.find((element) => element.id);
+    
+    direcciones = direcciones.filter((direccionesId)=>{
+        return direccionesId !== foundId;
     })
+    localStorage.setItem("direcciones",JSON.stringify(direcciones));
+    mostrarDirecciones();
+        
 }
+
+
+const botonEliminar = document.getElementsByName("eliminar");
+
+
+botonEliminar.forEach((item)=>{
+    item.addEventListener("click",eliminar)    
+})
+    //localStorage.setItem("direcciones", JSON.stringify(direcciones));
+
+
